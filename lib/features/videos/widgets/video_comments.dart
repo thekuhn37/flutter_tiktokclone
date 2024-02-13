2576ddb8bb2c43c2da407651ad565b9f2424ab79
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-02-13 19:49:51 +0900

    8.1 Comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokclone/constants/gaps.dart';
 import 'package:tiktokclone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -33,11 +34,72 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+            vertical: Sizes.size20,
+          ),
+          separatorBuilder: (context, index) => Gaps.v10,
           itemCount: 10,
-          itemBuilder: ((context, index) => Container(
-                child: const Text('I am a comment'),
+          itemBuilder: ((context, index) => Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 18,
+                    child: Text('곤르'),
+                  ),
+                  Gaps.h10,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '곤르',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size14,
+                            color: Colors.grey.shade500,
+                          ),
+                        ),
+                        Gaps.v3,
+                        const Text(
+                            'That travel must be very fun! Hope you enjoy the travel with chill.'),
+                      ],
+                    ),
+                  ),
+                  Gaps.h10,
+                  Column(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        color: Colors.grey.shade500,
+                        size: Sizes.size20,
+                      ),
+                      Gaps.v3,
+                      Text(
+                        '52.5K',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               )),
+        ),
+        bottomNavigationBar: const BottomAppBar(
+          elevation: 0,
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+                child: Text('호르'),
+              )
+            ],
+          ),
         ),
       ),
     );
