3aec5978bd0787f4636ff26e862a5729609b3c2b
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-02-17 11:45:54 +0900

    9.3 GridView

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -43,7 +43,23 @@ class DiscoverScreen extends StatelessWidget {
           ),
         ),
         body: TabBarView(children: [
-          for (var tab in tabs)
+          GridView.builder(
+            padding: const EdgeInsets.all(Sizes.size6),
+            itemCount: 20,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: Sizes.size10,
+              mainAxisSpacing: Sizes.size10,
+              childAspectRatio: 9 / 16,
+            ),
+            itemBuilder: (context, index) => Container(
+              color: Colors.teal,
+              child: Center(
+                child: Text("$index"),
+              ),
+            ),
+          ),
+          for (var tab in tabs.skip(1))
             Center(
               child: Text(
                 tab,
