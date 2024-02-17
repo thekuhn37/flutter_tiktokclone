034c55b5dafa6b49df798eabdd83a8e61409eec9
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-02-17 17:15:45 +0900

    9.4 Grid Item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokclone/constants/gaps.dart';
 import 'package:tiktokclone/constants/sizes.dart';
 
 final tabs = [
@@ -44,21 +49,73 @@ class DiscoverScreen extends StatelessWidget {
         ),
         body: TabBarView(children: [
           GridView.builder(
-            padding: const EdgeInsets.all(Sizes.size6),
-            itemCount: 20,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: Sizes.size10,
-              mainAxisSpacing: Sizes.size10,
-              childAspectRatio: 9 / 16,
-            ),
-            itemBuilder: (context, index) => Container(
-              color: Colors.teal,
-              child: Center(
-                child: Text("$index"),
+              padding: const EdgeInsets.all(Sizes.size6),
+              itemCount: 20,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 22,
               ),
-            ),
-          ),
+              itemBuilder: (context, index) => Column(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 9 / 16,
+                        // width / height
+                        child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder: "assets/images/placeholder.jpg",
+                            image:
+                                "https://steelfitusa.com/cdn/shop/articles/The-Anatomy-of-the-Biceps.jpg?v=1660142064&width=1500"),
+                      ),
+                      Gaps.v14,
+                      const Text(
+                        "This is a very long caption. In Tiktok, people can leave their comments here..",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: Sizes.size16 + Sizes.size2,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Gaps.v10,
+                      DefaultTextStyle(
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.shade600,
+                        ),
+                        child: Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: 12,
+                              backgroundImage:
+                                  AssetImage("assets/images/profile.jpeg"),
+                            ),
+                            Gaps.h6,
+                            const Expanded(
+                              child: Text(
+                                "Harry.D.Hwang from Busan",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Gaps.h6,
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size16,
+                              color: Colors.grey.shade600,
+                            ),
+                            Gaps.h2,
+                            const Text(
+                              '2.5M',
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+              // Image.asset("assets/images/biceps.jpg"),
+              ),
           for (var tab in tabs.skip(1))
             Center(
               child: Text(
