731c7ccf072ddccb0fb984ddb2b30f3ec65d2446
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-02-26 15:06:03 +0900

    11.3 AnimatedList part 2

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktokclone/constants/sizes.dart';
+import 'package:tiktokclone/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -14,13 +17,82 @@ class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
   final List<int> _items = [];
+  final Duration _duration = const Duration(milliseconds: 300);
 
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(_items.length);
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: _duration,
+      );
       //we can adjust the duration of the insertItem method.
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        duration: _duration,
+        (context, animation) => SizeTransition(
+            sizeFactor: animation,
+            child: Container(
+              color: Colors.red,
+              child: _makeTile(index),
+            )
+            // this is a widget that the user will see during the
+            // item is being deleted.
+            ),
+      );
+      _items.remove(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/107166856?s=96&v=4"),
+        child: Text('HG'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            'AntonioBM ($index)',
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            '2:16 PM',
+            style: TextStyle(
+              fontSize: Sizes.size12,
+              color: Colors.grey.shade500,
+            ),
+          ),
+        ],
+      ),
+      subtitle: Text(
+        'Hi, how are you?',
+        style: TextStyle(
+          color: Colors.grey.shade600,
+        ),
+      ),
+    );
   }
 
   @override
@@ -45,42 +117,8 @@ class _ChatsScreenState extends State<ChatsScreen> {
           return FadeTransition(
             key: UniqueKey(),
             opacity: animation,
-            child: SizeTransition(
-              sizeFactor: animation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  radius: 30,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/107166856?s=96&v=4"),
-                  child: Text('HG'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      'AntonioBM ($index)',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      '2:16 PM',
-                      style: TextStyle(
-                        fontSize: Sizes.size12,
-                        color: Colors.grey.shade500,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: Text(
-                  'Hi, how are you?',
-                  style: TextStyle(
-                    color: Colors.grey.shade600,
-                  ),
-                ),
-              ),
-            ),
+            child:
+                SizeTransition(sizeFactor: animation, child: _makeTile(index)),
           );
         },
       ),
