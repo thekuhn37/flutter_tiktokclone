c43e84a97889bf6de742a4cc7825611f53e060d0
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-02-26 14:33:57 +0900

    11.2 AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -11,6 +11,18 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(_items.length);
+      //we can adjust the duration of the insertItem method.
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,48 +33,56 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text('Direct Messages'),
         actions: [
           IconButton(
-              onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.plus))
+              onPressed: _addItem, icon: const FaIcon(FontAwesomeIcons.plus))
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/107166856?s=96&v=4"),
-              child: Text('HG'),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  'AntonioBM',
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/107166856?s=96&v=4"),
+                  child: Text('HG'),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'AntonioBM ($index)',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '2:16 PM',
+                      style: TextStyle(
+                        fontSize: Sizes.size12,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: Text(
+                  'Hi, how are you?',
                   style: TextStyle(
-                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade600,
                   ),
                 ),
-                Text(
-                  '2:16 PM',
-                  style: TextStyle(
-                    fontSize: Sizes.size12,
-                    color: Colors.grey.shade500,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: Text(
-              'Hi, how are you?',
-              style: TextStyle(
-                color: Colors.grey.shade600,
               ),
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
