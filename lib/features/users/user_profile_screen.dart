57b6219e5a9c346c0a651c9ccca6dc910e4aebfd
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-03-01 16:16:25 +0900

    12.5 Vertical Divider & Code Challenge

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktokclone/constants/gaps.dart';
 import 'package:tiktokclone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -15,113 +18,110 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          floating: true,
-          stretch: true,
-          // snap: true,
-          pinned: true,
-          backgroundColor: Colors.teal,
-          collapsedHeight: 60,
-          expandedHeight: 180,
-          flexibleSpace: FlexibleSpaceBar(
-            titlePadding: const EdgeInsets.symmetric(
-              horizontal: Sizes.size20,
+          title: const Text('Harry.D'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const FaIcon(
+                FontAwesomeIcons.gear,
+                size: Sizes.size20,
+              ),
             ),
-            // I don't know why but without this padding, the text is not on center.
-            stretchModes: const [
-              StretchMode.blurBackground,
-              // StretchMode.fadeTitle,
-              StretchMode.zoomBackground,
-            ],
-            background: Image.asset(
-              "assets/images/placeholder.jpg",
-              fit: BoxFit.cover,
-            ),
-            title: const Text('hello'),
-          ),
+          ],
         ),
-        const SliverToBoxAdapter(
+        SliverToBoxAdapter(
           child: Column(
             children: [
-              CircleAvatar(
-                backgroundColor: Colors.red,
-                radius: 40,
+              const CircleAvatar(
+                radius: 60,
+                foregroundColor: Colors.blue,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/107166856?s=96&v=4",
+                ),
+                child: Text('Harry.D.Hwang'),
+              ),
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    '@HarrythePassion',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size16,
+                    ),
+                  ),
+                  Gaps.h5,
+                  FaIcon(
+                    FontAwesomeIcons.solidCircleCheck,
+                    size: Sizes.size16,
+                    color: Colors.blue.shade500,
+                  )
+                ],
+              ),
+              Gaps.v24,
+              SizedBox(
+                height: Sizes.size48,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const UserInfo(numb: '97', info: 'Following'),
+                    VerticalDivider(
+                      width: Sizes.size40,
+                      thickness: 1,
+                      color: Colors.grey.shade300,
+                      indent: Sizes.size14,
+                      endIndent: Sizes.size14,
+                    ),
+                    const UserInfo(numb: '10.5M', info: 'Followers'),
+                    VerticalDivider(
+                      width: Sizes.size48,
+                      thickness: 1,
+                      color: Colors.grey.shade300,
+                      indent: Sizes.size14,
+                      endIndent: Sizes.size14,
+                    ),
+                    const UserInfo(numb: '149.3M', info: 'Likes'),
+                  ],
+                ),
               )
             ],
           ),
-        ),
-        SliverFixedExtentList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: 50,
-              (context, index) => Container(
-                color: Colors.teal[100 * (index % 9)],
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text('Item $index'),
-                ),
-              ),
-            ),
-            itemExtent: 100),
-        SliverPersistentHeader(
-          delegate: CustomDelegate(),
-          pinned: true,
-        ),
-        SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              childCount: 50,
-              (context, index) => Container(
-                color: Colors.purple[100 * (index % 9)],
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text('Item $index'),
-                ),
-              ),
-            ),
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 100,
-              mainAxisSpacing: Sizes.size10,
-              crossAxisSpacing: Sizes.size20,
-              childAspectRatio: 1,
-            ))
+        )
       ],
     );
   }
 }
 
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  // it requires many required methods and getters. So use code action to make them
-  // at one time.
+class UserInfo extends StatelessWidget {
+  final String numb;
+  final String info;
+
+  const UserInfo({
+    super.key,
+    required this.numb,
+    required this.info,
+  });
+
   @override
-  // this build method will return what our users will see.
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.indigo,
-      child: const FractionallySizedBox(
-        heightFactor: 1,
-        child: Center(
-          child: Text(
-            'Title!',
-            style: TextStyle(
-              color: Colors.white,
-            ),
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          numb,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: Sizes.size18,
           ),
         ),
-      ),
-      // it takes the largest space as much as possilbe at the parent's widget.
+        Gaps.v1,
+        Text(
+          info,
+          style: TextStyle(
+            color: Colors.grey.shade500,
+          ),
+        ),
+      ],
     );
-  }
-
-  @override
-  double get maxExtent => 150;
-  // like 'max Height'
-
-  @override
-  double get minExtent => 80;
-  // like 'min Hieght
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    // we will learn later when the return is true at DarkMode.
-    return false;
   }
 }
